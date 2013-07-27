c9ade3c2edc7a99a7b3fb6f6672acb974bc14827
Jonathan Sundquist <jsundquist@gmail.com>
2013-07-27 17:44:02 -0500

    Adding in a demo of how to take in paramters in the path. Also added in the htaccess file

--- web/index.php
+++ web/index.php
@@ -3,8 +3,17 @@ require_once '../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->register(new Silex\Privider\TwigServiceProvider(), array(
+  'twig.path' => '../views',
+));
+
 $app->get('/', function(){
     return 'hello world';
 });
 
+$app->get('/hello/{name}', function($name) use ($app) {
+    return $app['twig']->render('hello.twig', array(
+        'name'=>$name,
+    ));
+});
 $app->run();
